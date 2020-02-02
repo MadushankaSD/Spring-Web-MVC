a40b3afa8199dba27e1a53fd75c04c81f4e60926
madushanka kumara <oska.coc@gmail.com>
2020-02-02 14:09:40 +0530

    Add Controller clases and Annotate them

--- src/main/java/io/github/madushanka/webmvc/controller/ItemController.java
+++ src/main/java/io/github/madushanka/webmvc/controller/ItemController.java
@@ -1,18 +1,11 @@
 package io.github.madushanka.webmvc.controller;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @CrossOrigin
-@RequestMapping("api/v1/hello")
-public class MyController {
-
-    @GetMapping
-    public String doSometing(){
-        return "Hello WebMVC";
-    }
-
+@RequestMapping("api/v1/items")
+public class ItemController {
 }
